748e44f25700b3e8f62325dac3e4d26105307043
joaogcbarbosa <joaogcb29@gmail.com>
2024-10-16 22:08:11 -0300

    messager senders

---
+++ app/utils/messager.py
@@ -0,0 +1,10 @@
+def sports_message(counter: int) -> str:
+    return f"Sports news {counter}"
+
+
+def news_message(counter: int) -> str:
+    return f"News news {counter}"
+
+
+def technology_message(counter: int) -> str:
+    return f"Technology news {counter}"
