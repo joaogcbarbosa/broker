bf2241f3cbcc2f82007b36397fae6fe763378756
joaogcbarbosa <joaogcb29@gmail.com>
2024-10-16 22:08:02 -0300

    main refactored

--- app/main.py
+++ app/main.py
@@ -1,41 +1,26 @@
-from queue import Queue
 from threading import Thread
+from queue import Queue
 from time import sleep
-
-from models.broker import Publisher, Subscriber
-from app.utils.worker import worker
-
+from random import randint
+from utils.messager import sports_message
+from utils.workers import sports_publisher, sports_subscriber
 
 def main():
-    sports_publisher = Publisher(id=1, topic="Sports")
-    news_publisher = Publisher(id=2, topic="News")
-    tech_publisher = Publisher(id=3, topic="Technology")
-    
     broker = Queue()
 
-    subscriber_01 = Subscriber(id=1, name="João", subscriptions=[tech_publisher, sports_publisher])
-    subscriber_02 = Subscriber(id=2, name="Gustavo", subscriptions=[tech_publisher])
-    subscriber_03 = Subscriber(id=3, name="Paulo", subscriptions=None)
-    subscriber_04 = Subscriber(id=4, name="Bernardo", subscriptions=[sports_publisher, news_publisher, tech_publisher])
+    sports_publisher_thread = Thread(target=sports_publisher, args=(broker,), daemon=True)
+    sports_subscriber_1_thread = Thread(target=sports_subscriber, args=("João",), daemon=True)
+    sports_subscriber_2_thread = Thread(target=sports_subscriber, args=("Gustavo",), daemon=True)
 
+    sports_publisher_thread.start()
+    sports_subscriber_1_thread.start()
+    sports_subscriber_2_thread.start()
 
-    message_counter = 1
+    sports_counter = 1
     while True:
-        sports_thread = Thread(
-            target=worker(topic=sports_publisher.topic, message_number=message_counter, broker_queue=broker)
-        )
-        news_thread = Thread(
-            target=worker(topic=news_publisher.topic, message_number=message_counter, broker_queue=broker)
-        )
-        tech_thread = Thread(
-            target=worker(topic=tech_publisher.topic, message_number=message_counter, broker_queue=broker)
-        )
-        sports_thread.start()
-        news_thread.start()
-        tech_thread.start()
-        print(broker.queue)
-        sleep(3)
-        message_counter += 1
+        broker.put(sports_message(sports_counter))
+        sports_counter += 1
+        sleep(4)
 
 
 if __name__ == "__main__":
