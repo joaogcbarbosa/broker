fd26978184e22ca2b744c9c2fea0d97392a9a646
joaogcbarbosa <joaogcb29@gmail.com>
2024-10-18 00:32:19 -0300

    Chore: working on

--- main.py
+++ main.py
@@ -1,32 +1,23 @@
 from threading import Thread
-from multiprocessing import Queue
+from multiprocessing import Queue, Process
 from random import choice
 from time import sleep
 
 TOPICS = ["Sports", "News", "Tech"]
-TOPICS = ["Sports", "News"]
+TOPICS = ["Sports"]
 
 class Broker(Thread):
     def __init__(self):
         Thread.__init__(self)
         self.queues = {t: Queue() for t in TOPICS}
 
-    def _deliver_messages(self, topic: str) -> str:
-        if topic == "Sports":
-            print(self.queues[topic].get())
-        elif topic == "News":
-            for msg in self.queues[topic]:
-                print(msg)
-        elif topic == "Tech":
-            for msg in self.queues[topic]:
-                print(msg)
+    def _deliver_messages(self, message: str) -> str:
+        return message
 
     def run(self):
         while True:
-            print(self.queues["Sports"].get())
-            print(self.queues["News"].get())
-            # self._deliver_messages(topic="Sports")
-            print("waiting")
+            self._deliver_messages(self.queues["Sports"].get())
+            sleep(3)
 
 
 def publisher_worker() -> None:
@@ -37,13 +28,13 @@ def publisher_worker() -> None:
             case "Sports":
                 broker.queues["Sports"].put(f"{topic_choice} {sports_counter}")
                 sports_counter += 1
-            case "News":
-                broker.queues["News"].put(f"{topic_choice} {news_counter}")
-                news_counter += 1
+            # case "News":
+            #     broker.queues["News"].put(f"{topic_choice} {news_counter}")
+            #     news_counter += 1
             # case "Tech":
             #     broker.queues["Tech"].put(f"{topic_choice} {tech_counter}")
             #     tech_counter += 1
-        sleep(4)
+        sleep(2)
 
 
 if __name__ == "__main__":
