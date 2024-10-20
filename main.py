330a7653df36f0609ca4cd1ac7965693c46d6e36
joaogcbarbosa <joaogcb29@gmail.com>
2024-10-20 05:20:28 -0300

    Chore: working on

--- main.py
+++ main.py
@@ -11,13 +11,12 @@ class Broker(Thread):
         Thread.__init__(self)
         self.queues = {t: Queue() for t in TOPICS}
 
-    def _deliver_messages(self, message: str) -> str:
-        return message
+    # def _deliver_messages(self, message: str) -> str:
+    #     return message
 
     def run(self):
         while True:
-            print(self.queues["Sports"].qsize())
-            print(self._deliver_messages(self.queues["Sports"].get()))
+            # print(self._deliver_messages(self.queues["Sports"].get()))
             sleep(3)
 
 
@@ -26,48 +25,44 @@ class Publisher(Thread):
         Thread.__init__(self)
 
     def run(self):
-        sports_counter = news_counter = tech_counter = 1
+        sports_counter = 1
         while True:
             topic_choice = choice(TOPICS)
             match topic_choice:
                 case "Sports":
                     broker.queues["Sports"].put(f"{topic_choice} {sports_counter}")
                     sports_counter += 1
-                # case "News":
-                #     broker.queues["News"].put(f"{topic_choice} {news_counter}")
-                #     news_counter += 1
-                # case "Tech":
-                #     broker.queues["Tech"].put(f"{topic_choice} {tech_counter}")
-                #     tech_counter += 1
+                    broker.queues["Sports"].put(None)
             sleep(2)
 
 
 class Subscriber(Process):
     def __init__(self, topic):
         Process.__init__(self)
-        self._topic = topic
+        self._topic: str = topic
 
     @property
     def topic(self):
         return self._topic
+    
+    def get_message(self, topic: str):
+        while True:
+            item = broker.queues[topic].get()
+            if item is None:
+                break
+            print(f"Consumindo {item}")
 
     def run(self):
         while True:
-            if self.topic == "Sports":
-                print("Listening Sports every 2 seconds")
-                sleep(2)
-            elif self.topic == "Tech":
-                print("Listening Tech every 1 second")
-                sleep(1)
+            self.get_message(self.topic)
+            sleep(2)
 
 
 if __name__ == "__main__":
-    # publisher = Publisher()
-    # broker = Broker()
+    publisher = Publisher()
+    broker = Broker()
     sports_subscriber = Subscriber(topic="Sports")
-    # tech_subscriber = Subscriber(topic="Tech")
-    sports_subscriber.start()
-    tech_subscriber.start()
 
-    # publisher.start()
-    # broker.start()
+    sports_subscriber.start()
+    publisher.start()
+    broker.start()
